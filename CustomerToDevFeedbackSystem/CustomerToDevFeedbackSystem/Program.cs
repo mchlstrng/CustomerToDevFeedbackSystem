e15fd03d0ec4a6d6ba06de681c080518d09d722f
Michael Strange <michaelstrange@live.dk>
2024-05-29 11:20:52 +0200

    navmenu now loads fake data from "INavmenuService".

--- CustomerToDevFeedbackSystem/CustomerToDevFeedbackSystem/Program.cs
+++ CustomerToDevFeedbackSystem/CustomerToDevFeedbackSystem/Program.cs
@@ -1,10 +1,13 @@
 using CustomerToDevFeedbackSystem.Components;
+using CustomerToDevFeedbackSystem.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddTransient<INavmenuService, NavmenuService>();
 
 var app = builder.Build();
 
---
+++ CustomerToDevFeedbackSystem/CustomerToDevFeedbackSystem/Services/NavmenuService.cs
@@ -0,0 +1,30 @@
+﻿using static CustomerToDevFeedbackSystem.Services.NavmenuService;
+
+namespace CustomerToDevFeedbackSystem.Services
+{
+    public interface INavmenuService
+    {
+        List<NavMenuItem> GetAllProjects();
+    }
+
+    public class NavmenuService : INavmenuService
+    {
+        public List<NavMenuItem> GetAllProjects()
+        {
+            return
+            [
+                new NavMenuItem { Id = 1, Title = "Project 1" },
+                new NavMenuItem { Id = 2, Title = "Project 2" },
+                new NavMenuItem { Id = 3, Title = "Project 3" },
+                new NavMenuItem { Id = 4, Title = "Project 4" },
+            ];
+        }
+
+        public class NavMenuItem
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Url => $"/project/{Id}";
+        }
+    }
+}
